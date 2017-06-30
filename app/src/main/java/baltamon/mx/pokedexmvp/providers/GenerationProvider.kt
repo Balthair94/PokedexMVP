fe5ad48f2f8143e91b16b40dd294491a5d4a23e8
Balthair94 <archerbaltazar@gmail.com>
2017-06-30 03:51:28 -0500

    improve GenerationProvider

--- app/src/main/java/baltamon/mx/pokedexmvp/providers/GenerationProvider.kt
+++ app/src/main/java/baltamon/mx/pokedexmvp/providers/GenerationProvider.kt
@@ -1,7 +1,6 @@
 package baltamon.mx.pokedexmvp.providers
 
 import baltamon.mx.pokedexmvp.models.Generation
-import baltamon.mx.pokedexmvp.presenters.MainActivityPresenter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -9,22 +8,20 @@ import retrofit2.Response
 /**
  * Created by Baltazar Rodriguez on 28/06/2017.
  */
-class GenerationProvider (val presenter: MainActivityPresenter){
+class GenerationProvider (val presenter: GenerationProviderInterface){
 
     fun loadPokemonGeneration(){
-
-        val inter: GenerationProviderInterface = presenter
-
         val restClient = buildRetrofit(buildGson()).create(PokeAPIClient::class.java)
         val call = restClient.generation
 
         call.enqueue(object : Callback<Generation>{
-            override fun onFailure(call: Call<Generation>?, t: Throwable?) = inter.onFailure(t.toString())
+            override fun onFailure(call: Call<Generation>?, t: Throwable?) =
+                    presenter.onFailure(t.toString())
 
             override fun onResponse(call: Call<Generation>?, response: Response<Generation>) {
                 when (response.code()){
                     200 -> {
-                        inter.onSuccess(response.body()!!)
+                        presenter.onSuccess(response.body()!!)
                     }
                 }
             }
---
+++ app/src/main/java/baltamon/mx/pokedexmvp/viewholders/CVViewHolder.kt
@@ -0,0 +1,7 @@
+package baltamon.mx.pokedexmvp.viewholders
+
+/**
+ * Created by Baltazar Rodriguez on 30/06/2017.
+ */
+class CVViewHolder {
+}
